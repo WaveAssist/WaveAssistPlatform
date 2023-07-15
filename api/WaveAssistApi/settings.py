cf02dc7456b2efb758c4c00e1f28bf86999ace40
Kakshil Shah <kakshil.shah@gmail.com>
2023-07-16 01:25:16 +0700

    api: Admin

--- api/WaveAssistApi/settings.py
+++ api/WaveAssistApi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'REMOVED_CREDENTIAL'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['wavepredictbackend.us-east-1.elasticbeanstalk.com','*','api.wavepredict.com']
