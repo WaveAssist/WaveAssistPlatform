a159c4154ebfc51f1a2e157cd8740326fc784588
Kakshil Shah <kakshil.shah@gmail.com>
2023-07-15 21:23:39 +0700

    api: No compile

--- api/WaveAssistApiApp/engine_views.py
+++ api/WaveAssistApiApp/engine_views.py
@@ -52,10 +52,10 @@ def generate_python_code_text(node_array):
         for node_object in node_array:
             python_code = node_object.python_code
 
-            try:
-                compile(python_code, '<string>', 'exec')
-            except Exception as e:
-                return False, 'Python code is not valid for node: ' + node_object.node_key + " Error: " + str(e)
+            # try:
+            #     compile(python_code, '<string>', 'exec')
+            # except Exception as e:
+            #     return False, 'Python code is not valid for node: ' + node_object.node_key + " Error: " + str(e)
 
 
             ##Create a python code text which has the python_code of each node as a function, with the function name as the node_key
@@ -79,10 +79,10 @@ def generate_python_code_text(node_array):
 
 
         ##Validate if the entire code is proper python code, if not, return error
-        try:
-            compile(python_code_text, '<string>', 'exec')
-        except Exception as e:
-            return False, "Error in compiling code: " + str(e)
+        # try:
+        #     compile(python_code_text, '<string>', 'exec')
+        # except Exception as e:
+        #     return False, "Error in compiling code: " + str(e)
 
 
         return True, python_code_text
