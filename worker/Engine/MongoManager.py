09919dd5976c1742f20af66eebaf7e4c166f8c0f
Kakshil Shah <kakshil.shah@gmail.com>
2023-08-03 21:06:21 +0530

    worker: MongoManager

--- worker/Engine/MongoManager.py
+++ worker/Engine/MongoManager.py
@@ -47,7 +47,6 @@ class MongoManager:
             collection = self.database[io_data_key]
             ##Update the entire document with the new data
             replace_result = collection.replace_one({search_key: search_value}, data_dict)
-            print(replace_result)
             if replace_result.modified_count > 0:
                 return True
             else:
