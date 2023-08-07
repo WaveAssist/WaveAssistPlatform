14160dc56c00faddeb00b69ec068beb08a8b13b7
Kakshil Shah <kakshil.shah@gmail.com>
2023-08-07 20:23:55 +0530

    api: ID enabled

--- api/WaveAssistApiApp/views.py
+++ api/WaveAssistApiApp/views.py
@@ -69,7 +69,7 @@ def load_project_data(request):
         data_format_array = []
         for io_data_object in io_data_array:
             data_array = mongo_manager.fetch_data(io_data_object.key)
-            data_array = MongoManager.remove_id_from_array(data_array)
+            # data_array = MongoManager.remove_id_from_array(data_array)
             data_array = MongoManager.manage_na(data_array)
             if io_data_object.output_type == 2:
                 ##Sort data array's by row key in dict and then column key
