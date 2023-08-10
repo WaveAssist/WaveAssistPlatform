cc9f4b8b7ec5e70d93bccfee5abb972c03c01cdc
Kakshil Shah <kakshil.shah@gmail.com>
2023-08-10 18:37:04 +0530

    api: Engine change

--- api/WaveAssistApiApp/engine_views.py
+++ api/WaveAssistApiApp/engine_views.py
@@ -52,7 +52,9 @@ def load_node_data(request):
 
 def generate_python_code_text(node_array):
     try:
-        python_code_text = ''
+        python_code_text = 'from Integrations.Zerodha import * \n from Integrations.IB import * \n'
+
+
         for node_object in node_array:
             python_code = node_object.python_code
 
