eaecaba897cf10cb63f0c055b7f9bef574331eb5
Kakshil Shah <kakshil.shah@gmail.com>
2023-08-30 13:15:32 +0530

    worker: Back to original

--- worker/Integrations/IB.py
+++ worker/Integrations/IB.py
@@ -14,7 +14,7 @@ logging.getLogger("ibapi").setLevel(logging.CRITICAL)  # Set level higher than C
 
 class IBapi(EWrapper, EClient):
     tick_data_dictionary = {}
-    last_refreshed = None
+
 
     def __init__(self):
         EClient.__init__(self, self)
@@ -23,8 +23,6 @@ class IBapi(EWrapper, EClient):
         super().tickPrice(reqId, tickType, price, attrib)
         if price == 0 or price is None:
             return
-
-        self.last_refreshed = datetime.now(ist)
 
         if tickType == 4:  ##Last Price
             if reqId in self.tick_data_dictionary:
@@ -44,36 +42,23 @@ class IBapi(EWrapper, EClient):
                 self.tick_data_dictionary[reqId] = {'ask_price': price}
 
 
-class IBConnection(object):
+def run_loop(app):
+    app.run()
 
-    def run_loop(self):
-        self.app.run()
 
-    def connect_ib(self):
-        client_id = random.randint(100, 1000)
-        ib_port = 7496
+def connect_ib(ib_url):
+    client_id = random.randint(100,1000)
+    ib_port = 7496
 
-        print("Connecting to IB: " + str(self.ib_url) + ":" + str(ib_port))
-        app = IBapi()
-        app.connect(self.ib_url, ib_port, client_id)
-        api_thread = threading.Thread(target=self.run_loop, daemon=True, args=(app,))
-        api_thread.start()
+    print("Connecting to IB: " + str(ib_url) + ":" + str(ib_port))
+    app = IBapi()
+    app.connect(ib_url, ib_port, client_id)
+    api_thread = threading.Thread(target=run_loop, daemon=True, args=(app,))
+    api_thread.start()
 
-        time.sleep(1)
+    time.sleep(1)
 
-        print("Did connect: " + str(app.isConnected()))
-        return app, api_thread
+    print("Did connect: " + str(app.isConnected()))
+    return app, api_thread
 
-    def __init__(self):
-        self.ib_url = '18.210.163.209'
-        self.app, self.api_thread = self.connect_ib()
-
-    def fetch_and_refresh_ib_app_if_needed(self):
-        if self.app.isConnected():
-            return self.app
-        else:
-            # Retry IB connection
-            self.app.disconnect()
-            self.app, self.api_thread = self.connect_ib()
-            time.sleep(2)
-            return self.app
+ib_app, api_thread = connect_ib("18.210.163.209")
