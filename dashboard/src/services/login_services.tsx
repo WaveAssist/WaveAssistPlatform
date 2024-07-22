c3ee97d5dcb40c1d064f342e93b9f847d076c6a6
Kakshil Shah <kakshil.shah@gmail.com>
2024-07-22 20:48:58 +0530

    dashboard: CICD

--- dashboard/src/services/login_services.tsx
+++ dashboard/src/services/login_services.tsx
@@ -2,6 +2,7 @@ import { callApi } from "./base_service";
 
 export const loginAPI = async (username: string, password: string): Promise<any> => {
 	var path = "login/";
+
 	const body = new URLSearchParams({ username: username, password: password });
 	return callApi(path, body);
 };
