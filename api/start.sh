aa72a380dfe77d4b44443f47c50e51c1c6ab8daf
Kakshil Shah <kakshil.shah@gmail.com>
2024-08-02 20:10:08 +0530

    api: Tests & Celery

--- api/start.sh
+++ api/start.sh
@@ -6,7 +6,7 @@ python manage.py migrate
 celery -A WaveAssistApi beat --loglevel=info &
 
 # Start Celery Flower
-celery -A WaveAssistApi flower --port=5555 &
+celery -A WaveAssistApi flower --port=5555 --basic_auth=wave:REMOVED_CREDENTIAL &
 
 # Start Gunicorn
 gunicorn --bind 0.0.0.0:8000 WaveAssistApi.wsgi:application
