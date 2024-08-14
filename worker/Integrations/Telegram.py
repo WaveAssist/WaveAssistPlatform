d083b7d5a50c5e86d83e07f30972936307494ca0
Kakshil Shah <kakshil.shah@gmail.com>
2024-08-14 21:56:11 +0530

    worker: Constants

--- worker/Integrations/Telegram.py
+++ worker/Integrations/Telegram.py
@@ -4,13 +4,13 @@ import time
 import pytz
 ist = pytz.timezone('Asia/Kolkata')
 from datetime import datetime
-
+from Utils.constants import *
 class Telegram:
     sent_messages = []
     MAX_MESSAGES_TO_STORE = 20
 
     async def send_telegram_message(self,user_id, message):
-        bot = Bot(token='REMOVED_CREDENTIAL') ##Can be put outside?
+        bot = Bot(token=TELEGRAM_BOT_TOKEN) ##Can be put outside?
         await bot.send_message(chat_id=user_id, text=message)
 
     def send_message(self, user_id, message, identifier=""):
