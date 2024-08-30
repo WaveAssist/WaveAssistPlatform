709d49694e2fc2b8770aeb662c9df4a9e6166e63
Kakshil Shah <kakshil.shah@gmail.com>
2024-08-30 15:57:43 +0530

    api: timeout

--- api/start.sh
+++ api/start.sh
@@ -9,4 +9,4 @@ celery -A WaveAssistApi beat --loglevel=info &
 celery -A WaveAssistApi flower --port=5555 --basic_auth=wave:REMOVED_CREDENTIAL &
 
 # Start Gunicorn
-gunicorn --bind 0.0.0.0:8000 WaveAssistApi.wsgi:application
+gunicorn --timeout 120 --bind 0.0.0.0:8000 WaveAssistApi.wsgi:application
