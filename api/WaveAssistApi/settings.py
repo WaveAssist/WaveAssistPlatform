9ca99a572bd3ac390f569614d3cfa469e2570f59
WaveAssist <kakshil.shah@waveassist.ai>
2024-10-26 00:40:26 +0530

    api: Update settings.py

--- api/WaveAssistApi/settings.py
+++ api/WaveAssistApi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'REMOVED_CREDENTIAL'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 IS_DOCKER=True
 
