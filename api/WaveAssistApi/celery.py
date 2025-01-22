24c2e3f86eee9458bbe35eb4a7a4c5170cc33674
Kakshil Shah <kakshil.shah@gmail.com>
2025-01-22 20:24:10 +0530

    api: Update 0025_rename_rabbitmq_queue_account_celery_queue_and_more.py, celery.py, deployment_views.py and related files

--- api/WaveAssistApi/celery.py
+++ api/WaveAssistApi/celery.py
@@ -5,15 +5,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WaveAssistApi.settings')
 from celery import Celery
 from django.conf import settings
 
-BROKER_URL = os.getenv('BROKER_URL', 'amqps://waveassist:REMOVED_CREDENTIAL@b-83686e9f-2c14-4878-91eb-96a9c4e00b8e.mq.us-east-1.amazonaws.com:5671')
-# BACKEND_URL = os.getenv('BACKEND_URL', BROKER_URL)
+REDIS_URL = os.getenv('REDIS_URL', 'redis://waveassistredis-lqjglu.serverless.use1.cache.amazonaws.com:6379/0')
+# REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 
 # Configure the Django version of the Celery app to use the same broker and backend
 app = Celery('WaveAssistApi',
-             broker=BROKER_URL
+             broker=REDIS_URL,
+             backend=REDIS_URL
              )
-
-
 # Load Celery settings from Django settings, the namespace 'CELERY' means all celery-related configuration keys
 # should have a `CELERY_` prefix in your Django settings file
 app.config_from_object(settings, namespace='CELERY')
