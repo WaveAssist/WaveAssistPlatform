2dec56192e1ec8ee63633b00b82788356e79f191
WaveAssist <kakshil.shah@waveassist.ai>
2025-02-15 19:10:10 +0530

    api: Update celery.py

--- api/WaveAssistApi/celery.py
+++ api/WaveAssistApi/celery.py
@@ -5,8 +5,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WaveAssistApi.settings')
 from celery import Celery
 from django.conf import settings
 
-# REDIS_URL = os.getenv('REDIS_URL', 'redis://waveassistredis-lqjglu.serverless.use1.cache.amazonaws.com:6379/0')
-REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+REDIS_URL = os.getenv('REDIS_URL', 'redis://waveassistredis-lqjglu.serverless.use1.cache.amazonaws.com:6379/0')
+# REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 
 # Configure the Django version of the Celery app to use the same broker and backend
 app = Celery('WaveAssistApi',
