849a7fc1188f6583dc2b42b54a7ac9ea50c03d9e
Kakshil Shah <kakshil.shah@gmail.com>
2025-02-25 09:58:52 +0530

    dashboard: New Login

--- dashboard/src/components/login_component.tsx
+++ dashboard/src/components/login_component.tsx
@@ -4,7 +4,8 @@ import WALogo from "../assets/Logo/Wave_Predict_W_Logo.png";
 import { useLocation, useNavigate } from "react-router-dom";
 import { auth, googleProvider, xProvider } from "../components/firebase";
 import { signInWithPopup, signInWithRedirect, getRedirectResult } from "firebase/auth";
-import { loginAPI } from "../services/login_services";
+import { loginAPI, getStartedAPI } from "../services/login_services";
+import { Spinner } from "react-bootstrap"; // Assuming you're using Bootstrap
 
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
@@ -15,6 +16,8 @@ const LoginComponent: React.FC = () => {
 	const [showGetStarted, setShowGetStarted] = useState(false);
 	const handleClose = () => setShowGetStarted(false);
 	const handleShow = () => setShowGetStarted(true);
+	const [loading, setLoading] = useState<boolean>(false);
+	const [loaderMessage, setLoaderMessage] = useState("");
 
 	useEffect(() => {
 		const uid = localStorage.getItem("uid");
@@ -25,11 +28,13 @@ const LoginComponent: React.FC = () => {
 
 	const handleSuccessfulSignIn = async (user: any) => {
 		try {
+			setLoading(true);
 			localStorage.setItem("user_data", JSON.stringify(user));
 			console.log(user);
 			console.log("Login successful! Redirecting to /manage");
 			var firebase_token = user.accessToken;
 			const data = await loginAPI(firebase_token);
+			setLoading(false);
 			//Check if data has key action
 			if (data.action === "PERFORM_GET_STARTED") {
 				handleShow();
@@ -38,6 +43,7 @@ const LoginComponent: React.FC = () => {
 				localStorage.setItem("user_data", JSON.stringify(data.user_data));
 				localStorage.setItem("project_array", JSON.stringify(data.project_array));
 				localStorage.setItem("uid", data.user_data.uid);
+				localStorage.setItem("firebase_uid", firebase_token);
 				//Get from location state
 				const from_location = location.state as any;
 				if (from_location) {
@@ -53,14 +59,31 @@ const LoginComponent: React.FC = () => {
 	};
 
 	const handleGetStarted = async () => {
-		return;
+		try {
+			setLoading(true);
+			setLoaderMessage("Setting up your account, please wait..");
+			handleClose();
+			var firebase_uid = localStorage.getItem("firebase_uid");
+			const data = await getStartedAPI(firebase_uid);
+			localStorage.setItem("user_data", JSON.stringify(data.user_data));
+			localStorage.setItem("project_array", JSON.stringify(data.project_array));
+			localStorage.setItem("uid", data.user_data.uid);
+			setLoading(false);
+			setLoaderMessage("");
+			navigate("/manage");
+		} catch (error) {
+			console.error("Get Started Failed:", error);
+			alert("Something went wrong creating your account, please try again..");
+		}
 	};
 
 	const handleGoogleSignIn = async () => {
 		try {
+			setLoading(true);
 			const result = await signInWithPopup(auth, googleProvider);
 			handleSuccessfulSignIn(result.user);
 		} catch (error: any) {
+			setLoading(false);
 			if (error.code === "auth/popup-blocked") {
 				console.log("Popup blocked, trying redirect...");
 				try {
@@ -82,10 +105,13 @@ const LoginComponent: React.FC = () => {
 
 	const handleXSignIn = async () => {
 		try {
+			setLoading(true);
 			// Attempt sign-in with popup
 			const result = await signInWithPopup(auth, xProvider);
 			handleSuccessfulSignIn(result.user);
+			setLoading(false);
 		} catch (error: any) {
+			setLoading(false);
 			if (error.code === "auth/invalid-credential") {
 				console.error("Invalid credentials. Please check your API keys and secrets.");
 				alert("Authentication failed. Please try again or contact support.");
@@ -129,6 +155,19 @@ const LoginComponent: React.FC = () => {
 					<h2 className="title-message">WaveAssist Management Console</h2>
 				</div>
 			</div>
+
+			{loading ? (
+				<div className="loader-container d-flex justify-content-center align-items-center pb-4">
+					<div className="text-center">
+						<Spinner animation="border" role="status" variant="success">
+							<span className="visually-hidden">Loading...</span>
+						</Spinner>
+						<p className="mt-2 text-success">{loaderMessage}</p>
+					</div>
+				</div>
+			) : (
+				<div></div>
+			)}
 
 			<div className="row justify-content-center">
 				<div className="col-8 col-md-5 col-lg-3 text-center">
--- dashboard/src/services/login_services.tsx
+++ dashboard/src/services/login_services.tsx
@@ -6,3 +6,9 @@ export const loginAPI = async (firebase_token: any): Promise<any> => {
 	const body = new URLSearchParams({ firebase_token: firebase_token });
 	return callApi(path, body);
 };
+
+export const getStartedAPI = async (firebase_token: any): Promise<any> => {
+	var path = "/manage/get_started/";
+	const body = new URLSearchParams({ firebase_token: firebase_token });
+	return callApi(path, body);
+};
