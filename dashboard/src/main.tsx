cfc007fae3ad669ab2b0428673681292ef9805b9
Kakshil Shah <kakshil.shah@gmail.com>
2025-05-02 20:33:27 +0530

    dashboard: posthog user

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -9,12 +9,11 @@ import { PostHogProvider } from "posthog-js/react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<PostHogProvider
-			apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+			apiKey="REMOVED_CREDENTIAL"
 			options={{
-				api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+				api_host: "https://us.i.posthog.com",
 				debug: import.meta.env.MODE === "development",
-			}}
-		>
+			}}>
 			<ToastProvider>
 				<App />
 			</ToastProvider>
