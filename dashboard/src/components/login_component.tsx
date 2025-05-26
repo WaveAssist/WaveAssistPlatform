b6ed6749eef208ad99fb7eb8cc29f0d94b0bc5a6
Kakshil Shah <kakshil.shah@gmail.com>
2025-05-26 15:46:22 +0530

    dashboard: CLI

--- dashboard/src/components/login_component.tsx
+++ dashboard/src/components/login_component.tsx
@@ -176,7 +176,7 @@ const LoginComponent: React.FC = () => {
 				<div className="col text-center mb-3" style={{ marginTop: "-20vh" }}>
 					<img src={WALogo} alt="WavePredict Logo" className="img-fluid mb-4 wp_logo_login" />
 					<h2 className="title-message">WaveAssist Management Console</h2>
-					<p className="title-message">This login flow was initiated from the CLI — complete it here to continue.</p>
+					{isCLILogin && <p className="title-message">This login flow was initiated from the CLI — complete it here to continue.</p>}
 				</div>
 			</div>
 
