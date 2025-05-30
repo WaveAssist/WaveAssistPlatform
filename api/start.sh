4ae89b8089e55a64df87696627163670b0e2c77a
Kakshil Shah <kakshil.shah@gmail.com>
2025-05-30 20:55:21 +0530

    api: Opentelemetry

--- api/start.sh
+++ api/start.sh
@@ -16,4 +16,5 @@ celery -A WaveAssistApi beat --loglevel=info &
 celery -A WaveAssistApi flower --port=5555 --basic_auth=admin:admin &
 
 # Start Gunicorn
-opentelemetry-instrument gunicorn --timeout 120 --bind 0.0.0.0:8000 WaveAssistApi.wsgi:application --workers 1
+#opentelemetry-instrument gunicorn --timeout 120 --bind 0.0.0.0:8000 WaveAssistApi.wsgi:application --workers 2
+gunicorn --timeout 120 --bind 0.0.0.0:8000 WaveAssistApi.wsgi:application --workers 2
