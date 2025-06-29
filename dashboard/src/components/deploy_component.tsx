1bf09ff98b2dfb1d723ad2b7aeba6a1f9a041b91
Darshika Joshi <joshidarshika88@gmail.com>
2025-06-29 18:14:50 +0530

    dashboard: timzonw

--- dashboard/src/components/deploy_component.tsx
+++ dashboard/src/components/deploy_component.tsx
@@ -59,6 +59,7 @@ const DeployComponent: React.FC = () => {
 			const formData = new FormData();
 			formData.append("repo_url", templateData.repo_url);
 			formData.append("uid", uid);
+			formData.append("timezone", Intl.DateTimeFormat().resolvedOptions().timeZone);
 			const response = await axios.post("https://api.waveassist.io/template/deploy_template/", formData, {
 				headers: { "Content-Type": "multipart/form-data" },
 			});
