ac878b3554f198eff9e94d94a9bd3c263e9e1a2b
Darshika Joshi <joshidarshika88@gmail.com>
2025-07-04 17:00:03 +0530

    api: Update start_beat.sh

--- api/start_beat.sh
+++ api/start_beat.sh
@@ -2,4 +2,5 @@
 # Run Django migrations
 export DJANGO_SETTINGS_MODULE=WaveAssistApi.settings
 ## Start Celery beat
+gunicorn --timeout 120 --bind 0.0.0.0:8000 WaveAssistApi.wsgi:application --workers 1 &
 celery -A WaveAssistApi beat --loglevel=info
