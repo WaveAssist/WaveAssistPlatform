d97da1e381b8152ef67af174b830599637acce82
Kakshil Shah <kakshil.shah@gmail.com>
2025-07-14 12:02:41 +0530

    api: BEAT

--- api/start_beat.sh
+++ api/start_beat.sh
@@ -2,5 +2,4 @@
 # Run Django migrations
 export DJANGO_SETTINGS_MODULE=WaveAssistApi.settings
 ## Start Celery beat
-gunicorn --timeout 120 --bind 0.0.0.0:8000 WaveAssistApi.wsgi:application --workers 1 &
-celery -A WaveAssistApi beat --loglevel=info
+celery -A WaveAssistApi beat --loglevel=DEBUG
