c89b6f83ac39a0f940f96b844498c791deed52e8
Kakshil Shah <kakshil.shah@gmail.com>
2025-08-28 19:03:52 +0530

    dashboard: Merge

--- dashboard/src/utils/private_route.tsx
+++ dashboard/src/utils/private_route.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { auth } from "../components/firebase";
-import { onAuthStateChanged } from "firebase/auth";
 
 interface PrivateRouteProps {
 	component: React.ComponentType<any>;
@@ -10,27 +8,7 @@ interface PrivateRouteProps {
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ component: Component, layout: Layout, ...rest }) => {
 	const location = useLocation();
-	const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-			setIsAuthenticated(!!user);
-
-			// Sync with localStorage for backward compatibility
-			if (user) {
-				localStorage.setItem("uid", user.uid);
-			} else {
-				localStorage.removeItem("uid");
-			}
-		});
-
-		return () => unsubscribe();
-	}, []);
-
-	// Show loading while checking authentication
-	if (isAuthenticated === null) {
-		return <div>Loading...</div>;
-	}
+	const isAuthenticated = !!localStorage.getItem("uid");
 
 	if (!isAuthenticated) {
 		return <Navigate to="/login" state={{ from: location }} />;
